eadd3b4c502c92b0cf6522189dba1f12810ad7fc
shabimoshood07 <86853613+shabimoshood07@users.noreply.github.com>
2022-01-19 21:38:23 +0100

    progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../src/logo.svg";
 function Navbar() {
+  const [user, setUser] = useState(false);
+
   return (
-    <div>
-      <h1>NAV</h1>
-    </div>
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <h1>ARTISAN</h1>
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <ul>
+          {!user ? (
+            <div className="">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Resister</Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <button>Logout</button>
+            </div>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
 }
 

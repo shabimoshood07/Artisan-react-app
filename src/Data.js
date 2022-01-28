918424396f2ad61a5d11ad9292f88fb69a2612df
shabimoshood07 <86853613+shabimoshood07@users.noreply.github.com>
2022-01-28 22:44:15 +0100

    progress

---
+++ src/Data.js
@@ -0,0 +1,59 @@
+const states = [
+  {
+    id: 0,
+    stateName: "Choose here",
+    statevalue: "Choose here",
+  },
+  {
+    id: 1,
+    stateName: "Abia",
+    statevalue: "Abia",
+  },
+  {
+    id: 2,
+    stateName: "Adamawa",
+    statevalue: "Adamawa",
+  },
+  {
+    id: 3,
+    stateName: "Akwa Ibom",
+    statevalue: "Akwa Ibom",
+  },
+  {
+    id: 4,
+    stateName: "Anambra",
+    statevalue: "Anambra",
+  },
+  {
+    id: 5,
+    stateName: "Bauchi",
+    statevalue: "Bauchi",
+  },
+  {
+    id: 6,
+    stateName: "Bayelsa",
+    statevalue: "Bayelsa",
+  },
+  {
+    id: 7,
+    stateName: "Benue",
+    statevalue: "Benue",
+  },
+  {
+    id: 8,
+    stateName: "kwara",
+    statevalue: "kwara",
+  },
+  {
+    id: 9,
+    stateName: "kano",
+    statevalue: "kano",
+  },
+  {
+    id: 10,
+    stateName: "Lagos",
+    statevalue: "Lagos",
+  },
+];
+
+export default states;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import states from "../Data";
 function Register() {
+  console.log(states);
   return (
     <div className="reg-body">
       <div className="reg-container">
@@ -48,44 +49,14 @@ function Register() {
             <div className="input-options">
               <label>--Select your State--</label>
               <select>
-                <option value="Choose here">Choose here</option>
-                <option value="Abia State">Abia State</option>
-                <option value="Adamawa State">Adamawa State</option>
-                <option value="Akwa Ibom State">Akwa Ibom State</option>
-                <option value="Anambra State">Anambra State</option>
-                <option value="Bauchi State">Bauchi State</option>
-                <option value="Bayelsa State">Bayelsea State</option>
-                <option value="Benue State">Benue State</option>
-                <option value="Borno State">Borno State</option>
-                <option value="Cross River State">Cross River State</option>
-                <option value="Delta State">Delta State</option>
-                <option value="Ebonyi State">Ebonyi State</option>
-                <option value="Edo State">Edo State</option>
-                <option value="Ekiti State">Ekiti State</option>
-                <option value="Enugu State">Enugu State</option>
-                <option value="Gombe State">Gombe State</option>
-                <option value="Imo State">Imo State</option>
-                <option value="Jigawa State">Jigawa State</option>
-                <option value="Kaduna State">Kaduna State</option>
-                <option value="Kano State">Kano State</option>
-                <option value="Katsina State">Katsina State</option>
-                <option value="Kebbi State">Kebbi State</option>
-                <option value="Kogi State">Kogi State</option>
-                <option value="Kwara State">Kwara State</option>
-                <option value="Lagos State">Lagos State</option>
-                <option value="Nasarawa State">Nasarawa State</option>
-                <option value="Niger State">Niger State</option>
-                <option value="Ogun State">Ogun State</option>
-                <option value="Ondo State">Ondo State</option>
-                <option value="Osun State">Osun State</option>
-                <option value="Oyo State">Oyo State</option>
-                <option value="Plateau State">Plateau State</option>
-                <option value="Rivers State">Rivers State</option>
-                <option value="Sokoto State">Sokoto State</option>
-                <option value="Taraba State">Taraba State</option>
-                <option value="Yobe State">Yobe State</option>
-                <option value="Zamfara State">Zamfara State</option>
-                <option value="FCT Abuja">FCT Abuja</option>
+                {states.map((eachstate) => {
+                  const { id, stateName, statevalue } = eachstate;
+                  return (
+                    <option key={id} value={statevalue}>
+                      {stateName}
+                    </option>
+                  );
+                })}
               </select>
               <label>--Profession--</label>
               <select>
@@ -108,11 +79,11 @@ function Register() {
             <input type="radio" name="gender" id="dot-2" />
             <span className="gender-title">Gender</span>
             <div className="category">
-              <label for="dot-1">
+              <label htmlFor="dot-1">
                 <span className="dot one"></span>
                 <span className="gender">Male</span>
               </label>
-              <label for="dot-2">
+              <label htmlFor="dot-2">
                 <span className="dot two"></span>
                 <span className="gender">Female</span>
               </label>

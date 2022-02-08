8cf2614a7f29eb7ade6c8d4d1909dcabd8804922
shabimoshood07 <86853613+shabimoshood07@users.noreply.github.com>
2022-02-08 17:10:15 +0100

    p

--- src/components/Artisan.js
+++ src/components/Artisan.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function Artisan({ _id, username, address, profession }) {
+function Artisan({ _id, username, address, profession, profileImage }) {
   return (
-    <section className="login-wrapper" key={_id}>
-      <h1>{username}</h1>
-      <h1>{address}</h1>
-      <h1>{profession}</h1>
-      <Link to={`/artisan/${_id}`}>Details</Link>
+    <section key={_id}>
+      <div className="artisan-wrapper">
+        <h1>{username}</h1>
+        <h1>{address}</h1>
+        <h1>{profession}</h1>
+        <div className="artisan-img-container">
+          <img
+            src={`http://localhost:5000/${profileImage}`}
+            className="profile-image"
+            alt="profile-image"
+          />
+        </div>
+        <Link to={`/artisan/${_id}`}>Details</Link>
+      </div>
     </section>
   );
 }
